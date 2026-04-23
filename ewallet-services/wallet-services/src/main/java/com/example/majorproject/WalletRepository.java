3c3a8d31c3e97a3eb7ecaa7be3efaf131f5a06f3
Aniket Khangar <aniketkhangar459@gmail.com>
2026-04-23 19:49:05 +0530

    quick fixes for redis integration

--- ewallet-services/wallet-services/src/main/java/com/example/majorproject/WalletRepository.java
+++ ewallet-services/wallet-services/src/main/java/com/example/majorproject/WalletRepository.java
@@ -3,7 +3,9 @@ package com.example.majorproject;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface WalletRepository extends JpaRepository<Wallet,Integer> {
 
 //    @Modifying
